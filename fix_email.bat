9c6b47736b5ca1006d927468fbfe57235424feac
ben-blazado <26291620+ben-blazado@users.noreply.github.com>
2021-03-19 11:02:08 -0600

    fixing attribution

---
+++ fix_email.bat
@@ -0,0 +1,12 @@
+git filter-branch --env-filter '
+    OLD_EMAIL="ben-blazado@gmail"
+    CORRECT_EMAIL="26291620+ben-blazado@users.noreply.github.com"
+    if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+    then
+        export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+    fi
+    if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+    then
+        export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+    fi
+    ' --tag-name-filter cat -- --branches --tags
